9691fbb915a77b304dad16a1a35cd137e0683e90
Natalia Banasiewicz <natalia.banasiewicz@wp.pl>
2020-01-20 13:30:38 +0100

    Add removing active classes in titleClickHandler

--- js/script.js
+++ js/script.js
@@ -4,12 +4,20 @@ const titleClickHandler = function(event){
     console.log('Link was clicked!');
     console.log(event);
   
-    /* remove class 'active' from all article links  */
+    /* remove class 'active' from all article links that have this class  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
+    for(let activeLink of activeLinks){
+      activeLink.classList.remove('active');
+    }
   
     /* add class 'active' to the clicked link */
   
-    /* remove class 'active' from all articles */
-  
+    /* remove class 'active' from all articles that have this class */
+    const activeArticles = document.querySelectorAll('.posts .post.active');
+    for(let activeArticle of activeArticles){
+        activeArticle.classList.remove('active');
+    }
+
     /* get 'href' attribute from the clicked link */
   
     /* find the correct article using the selector (value of 'href' attribute) */
