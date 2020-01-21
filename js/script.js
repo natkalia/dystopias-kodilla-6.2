8a5210a9803416eabc51d64b90ba85aac64e2a94
Natalia Banasiewicz <natalia.banasiewicz@wp.pl>
2020-01-21 14:34:20 +0100

    Add feature dynamic generating author name and filtering titles after clicking author link

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@ const optArticleSelector = '.post',
   optTitleListSelector = '.titles',
   optArticleTagsSelector = '.post-tags .list',
   optArticleTagsLinksSelector = '.post-tags .list a',
-  optAuthorSelector = '.post-author';
+  optArticleAuthorSelector = '.post-author',
+  optArticleAuthorLinksSelector = '.post-author a';
 
 const titleClickHandler = function(event) {
   event.preventDefault();
@@ -104,7 +105,7 @@ const tagClickHandler = (event) => {
   /* START LOOP: for each found tag link */
   for (let currentTagLink of currentTagLinks) {
   /* add class active */
-    currentTagLink.classList.remove('active');
+    currentTagLink.classList.add('active');
   /* END LOOP: for each found tag link */
   }
 
@@ -125,14 +126,57 @@ const addClickListenersToTags = () => {
 };
 addClickListenersToTags();
 
-
 const generateAuthors = () => {
   const articles = document.querySelectorAll(optArticleSelector);
   for (let article of articles) {
     const author = article.getAttribute('data-author');
-    const authorListing = article.querySelector(optAuthorSelector);
-    const authorHTML = `<a href="#">by ${author}</a>`;
+    const authorListing = article.querySelector(optArticleAuthorSelector);
+    const authorHTML = `<a href="#author-${author.replace(/\s+/g, '-')}">by ${author}</a>`;
     authorListing.innerHTML = authorHTML;
   }
 };
 generateAuthors();
+
+const authorClickHandler = (event) => {
+  event.preventDefault();
+
+  /* make new constant named "clickedElement" and give it the value of "this" */
+  const clickedElement = event.target;
+
+  /* make a new constant "href" and read the attribute "href" of the clicked element */
+  const href = clickedElement.getAttribute('href');
+
+  /* make a new constant and extract tag from the "href" constant */
+  const author = href.replace('#author-', '').replace(/-/g, ' ');
+
+  /* find author links with class active */
+  const activeAuthorLinks = document.querySelectorAll('a.active[href^="#author-"]');
+
+  /* START LOOP: for each active author link */
+  for (let activeAuthorLink of activeAuthorLinks) {
+    /* remove class active */
+    activeAuthorLink.classList.remove('active');
+  /* END LOOP: for each active tag link */
+  }
+
+  /* find all tag links with "href" attribute equal to the "href" constant */
+  const currentAuthorLinks = document.querySelectorAll('a[href="' + href + '"]');
+
+  /* START LOOP: for each found author link */
+  for (let currentAuthorLink of currentAuthorLinks) {
+  /* add class active */
+    currentAuthorLink.classList.add('active');
+  /* END LOOP: for each found tag link */
+  }
+
+  /* execute function "generateTitleLinks" with article selector as argument */
+  generateTitleLinks(`[data-author="${author}"]`);
+};
+
+const addClickListenersToAuthors = () => {
+  const authorLinks = document.querySelectorAll(optArticleAuthorLinksSelector);
+  authorLinks.forEach(link => {
+    link.addEventListener('click', authorClickHandler);
+  });
+};
+addClickListenersToAuthors();
